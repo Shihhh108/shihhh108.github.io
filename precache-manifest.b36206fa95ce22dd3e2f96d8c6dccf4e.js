1d100f5231c41132fd992c2609087fd6a807dc16
doge1e1 <allen900430@gmail.com>
2022-06-10 21:37:06 +0800

    Updates

---
+++ precache-manifest.b36206fa95ce22dd3e2f96d8c6dccf4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5d07512d64fe8511752",
+    "url": "/shihhh108.github.io/static/css/main.55a751b1.chunk.css"
+  },
+  {
+    "revision": "f5d07512d64fe8511752",
+    "url": "/shihhh108.github.io/static/js/main.f5d07512.chunk.js"
+  },
+  {
+    "revision": "fc3557f4d91b9107f886",
+    "url": "/shihhh108.github.io/static/js/1.fc3557f4.chunk.js"
+  },
+  {
+    "revision": "094ad3ca6aec4b29774c",
+    "url": "/shihhh108.github.io/static/js/2.094ad3ca.chunk.js"
+  },
+  {
+    "revision": "b058027e8101208d6447",
+    "url": "/shihhh108.github.io/static/js/runtime~main.b058027e.js"
+  },
+  {
+    "revision": "465365986e95298a656cf6f876c7272d",
+    "url": "/shihhh108.github.io/index.html"
+  }
+];
